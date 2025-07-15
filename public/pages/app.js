12e2e7b7856af882d23b29e94c440b0b846cc434
Franco Montenegro <franco@francomontenegro.com.ar>
2025-07-15 15:11:21 -0300

    simplify entry points.

--- public/pages/app.js
+++ public/pages/app.js
@@ -1,92 +1,45 @@
 const state = {
     page: "",
 
-    page_params: {},
+    number: 0,
 
     pages: {
-        "/checkout": checkout_init,
-        "/register": registration_init,
-        "/jokes": jokes_init,
-        // routes with parameters
-        // "user/:user_id/edit": user_edit_init,
+        "/": function() {
+            console.log("user navigated to /")
+
+            state.page = ""
+        },
+
+        "/user/:user_id/update": function({ user_id = "" }) {
+            console.log(`user navigated to /user/${user_id}/update`)
+
+            state.page = "user_update"
+        },
     },
-
-    value: 0,
-    
-    username: "john",
-
-    password: "doe",
-
-    registration: registration_state(),
-
-    checkout: checkout_state(),
-
-    jokes: jokes_state(),
 }
 
-function default_page_init() {
-    state.page = ""
+function increase_number() {
+    console.log("asdasd")
+    state.number++
 }
 
-async function set_username_after(ms = 0, new_username = "") {
-    await sleep_for(ms)
-    state.username = new_username
-}
-
-function demo_page() {
-    const update_user_link = link({
-        path: `/user/${42}/update`,
-        content: "go to user!"
-    })
-
+function number_component() {
     return `
-        <div>Username is: ${state.username}</div>
-        <div>Password is: ${state.password}</div>
-
-        <div>Value is: ${state.value}</div>
-
         <div>
-            <button 
-                type="button" 
-                onclick="state.username = 'foo'"
-            >
-                set username to foo
-            </button>
-
-            <button 
-                type="button" 
-                onclick="set_username_after(1000, 'bar')"
-            >
-                set username to bar after 1 second
-            </button>
-
-            <input 
-                type="text" 
-                oninput="state.username = event.target.value" 
-                value="${state.username}"
-            />
-        </div>
-
-        <div>${update_user_link}</div>
-                
-        <div>
-            ${link({
-                path: `/checkout`,
-                content: "go to checkout!"
-            })}
-        </div>
-    `
+            <div>number is: ${state.number}</div>
+            <button type="button" onclick="increase_number()">increase</button>
+        </div>`
 }
 
-function app() {
+function user_update_page() {
+    return `<div>user update page</div>`
+}
+
+function html_to_draw() {
     switch (state.page) {
-        case "checkout":
-            return checkout_page()
-        case "registration":
-            return registration_page()
-        case "jokes":
-            return jokes_page()
-        default:
-            return demo_page()
+        case "":
+            return `<div>hello world</div><div>${number_component()}</div>`
+        case "user_update":
+            return user_update_page()
     }
 }
